a4ddbf79c82f900b26b35c8d48826909ea4bfe58
Gbario <giuli_rio4@hotmail.com>
2024-10-29 01:08:47 +0100

    Agregado de funcionalidades

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var nombreEvento, numeroGanadores, titulo, premios, patrocinadores, selectGanadores;
+var nombreEvento, numeroGanadores, titulo, premios, patrocinadores, selectGanadores, nombreParticipante, dniParticipante, listaParticipantes, botonCrearEvento, botonEliminarEvento, botonCrearQR, botonAgregarParticipante, botonRealizarSorteo;
 
 function ingresarNombreEvento(){
     
     titulo = document.getElementById("titulo");
     nombreEvento = document.getElementById("nombre-evento").value;
     titulo.textContent = nombreEvento;
+    titulo.style.color = "Black";
 
     
     
@@ -14,6 +15,7 @@ function menuGanadores() {
     const contenedorPremios = document.getElementById("contenedorPremios");
     numeroGanadores = document.getElementById("cantidadGanadores").value;
     contenedorPremios.innerHTML = ("");
+    
     
     for(let i = 1; i <= numeroGanadores; i++){
         const inputPremios = document.createElement("div");
@@ -40,7 +42,25 @@ function menuGanadores() {
 }
 
 function agregarParticipante(){
+nombreParticipante = document.getElementById("nombreParticipante");
+dniParticipante = document.getElementById("dniParticipante");
 
+    if(nombreParticipante.value !== "" && dniParticipante.value !== ""){
+        listaParticipantes  = document.getElementById("listaParticipantes");
+        var nuevoParticipante = document.createElement("li");
+        nuevoParticipante.innerHTML = nombreParticipante.value + "  " + dniParticipante.value + "      " + `<button class="botonEliminarParticipante" onClick="eliminarParticipante(this)"> x </button>`;
+        listaParticipantes.appendChild(nuevoParticipante);
+        nombreParticipante.value = "";
+        dniParticipante.value = "";
+    }else{
+        alert("Nombre o DNI inválido")
+    }
+
+}
+
+function eliminarParticipante(cruz){
+    const participante = cruz.parentNode;
+    participante.remove();
 }
 
 function deshabilitarInput(input){
@@ -52,41 +72,77 @@ function habilitarInput(input){
 
 function crearEvento(){
     nombreEvento = document.getElementById("nombre-evento").value;
+    selectGanadores = document.getElementById("cantidadGanadores");
     if(nombreEvento === ""){
         alert("El nombre del evento no es válido");
         return;
     }else{
         ingresarNombreEvento();
     }
-    if(numeroGanadores === "0"){
+    if(selectGanadores.value === "0"){
         alert("Agrege al menos 1 premio");
         return;
     }
     premios = document.getElementsByClassName("premios");
     patrocinadores = document.getElementsByClassName("patrocinadores");
     for(let i = 0; i < (numeroGanadores); i++){
-        premios[i].disabled = true;
-        patrocinadores[i].disabled = true;
+        if(premios[i].value === ""){
+            alert("Premio " + (i+1) + " vacío");
+            return;
+        }
+        deshabilitarInput(premios[i]);
+       deshabilitarInput(patrocinadores[i]);
     }
-    selectGanadores = document.getElementById("cantidadGanadores");
+    
+   
     deshabilitarInput(selectGanadores);
-    const botonCrearEvento = document.getElementById("botonCrearEvento");
+    botonCrearEvento = document.getElementById("botonCrearEvento");
     deshabilitarInput(botonCrearEvento);
     nombreEvento = document.getElementById("nombre-evento");
     deshabilitarInput(nombreEvento);
-    const botonEliminarEvento = document.createElement("button");
+    botonEliminarEvento = document.createElement("button");
     botonEliminarEvento.id = "botonEliminarEvento";
-    botonEliminarEvento.onclick = eliminarEvento;
+    botonEliminarEvento.onclick = cuadroConfirmacionEliminarEvento;
     botonEliminarEvento.className = "botonEliminar";
     botonEliminarEvento.textContent = "Eliminar Evento";
     const contenedorBotonesEvento = document.getElementById("seccion-nombre");
     contenedorBotonesEvento.appendChild(botonEliminarEvento);
+
+    botonCrearQR = document.getElementById("botonCrearQR"); // Habilitar inputs y botones luego de crear sorteo
+    habilitarInput(botonCrearQR);
+    
+    botonAgregarParticipante = document.getElementById("botonAgregarParticipante");
+    habilitarInput(botonAgregarParticipante);
+
+    botonRealizarSorteo = document.getElementById("botonRealizarSorteo");
+    habilitarInput(botonRealizarSorteo);
+
+    nombreParticipante = document.getElementById("nombreParticipante");
+    dniParticipante = document.getElementById("dniParticipante");
+    habilitarInput(nombreParticipante);
+    habilitarInput(dniParticipante);
+    
+    
 }
  
+function crearQR(){
+
+}
+
+function cuadroConfirmacionEliminarEvento(){
+   var cuadroConfirmacion = document.getElementById("cuadroConfirmacion");
+    cuadroConfirmacion.style.display = "block";
+}
+function ocultarCuadroConfirmacionEliminarEvento(){
+    var cuadroConfirmacion = document.getElementById("cuadroConfirmacion");
+    cuadroConfirmacion.style.display = "none";
+}
+
 function eliminarEvento(){
 nombreEvento.value = "";
 habilitarInput(nombreEvento);
-titulo.innerHTML = "";
+titulo.textContent = "Crear sorteo";
+titulo.style.color = "gray";
 numeroGanadores.value = "";
 habilitarInput(selectGanadores);
 for(let i = 0; i < (numeroGanadores); i++){
@@ -97,4 +153,11 @@ for(let i = 0; i < (numeroGanadores); i++){
 }
 habilitarInput(botonCrearEvento);
 botonEliminarEvento.remove();
+deshabilitarInput(botonAgregarParticipante);
+deshabilitarInput(botonCrearQR);
+deshabilitarInput(botonRealizarSorteo);
+deshabilitarInput(nombreParticipante);
+deshabilitarInput(dniParticipante);
+listaParticipantes.innerHTML = ("");
+ocultarCuadroConfirmacionEliminarEvento()
 }
